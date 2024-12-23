bc11290e89d4bbd35c80b4a98247eadaee9e8e2f
WaelNalouti <wael.nalouti@gmail.com>
2024-12-23 02:59:45 +0100

    fix(nvim/telescope): config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,34 +1,34 @@
 return {
   "telescope.nvim",
+  config = function()
+    require("telescope").load_extension("fzf")
+    require("telescope").setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            width = 0.95,
+            height = 0.95,
+          },
+          vertical = {
+            width = 0.95,
+            height = 0.95,
+            preview_height = 0.6,
+          },
+        },
+      },
+    })
+  end,
+  keys = {
+    {
+      "<leader>sx",
+      require("telescope.builtin").resume,
+      desc = "Resume Telescope",
+      mode = { "n" },
+    },
+  },
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
-    keys = {
-      {
-        "<leader>sx",
-        require("telescope.builtin").resume,
-        desc = "Resume Telescope",
-        mode = { "n" },
-      },
-    },
-    config = function()
-      require("telescope").load_extension("fzf")
-      require("telescope").setup({
-        defaults = {
-          layout_strategy = "horizontal",
-          layout_config = {
-            horizontal = {
-              width = 0.95,
-              height = 0.95,
-            },
-            vertical = {
-              width = 0.95,
-              height = 0.95,
-              preview_height = 0.6,
-            },
-          },
-        },
-      })
-    end,
   },
 }
